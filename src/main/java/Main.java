3565574ccfad197f1b97389c2e2bb7935e6cb2bb
Unknown <julius.cc@hotmail.com>
2018-10-11 13:17:05 +0200

    Added user service department manager

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,7 @@ public class Main {
         loginManager.addUser("adm", "1234", UserRole.AdministrationDepartmentManager);
         loginManager.addUser("sdm", "1234", UserRole.ServiceDepartmentMember);
         loginManager.addUser("pdman", "1234", UserRole.ProductionDepartmentManager);
+        loginManager.addUser("sdman", "1234", UserRole.ServiceDepartmentManager);
     }
 
     private static void addMenuItems(Menu mainMenu, LoginManager loginManager) {
